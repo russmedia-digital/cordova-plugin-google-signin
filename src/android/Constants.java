5d5499b095ae917a6dbd1b340b32eb016ae86f27
Mahendra Liya <liyamahendra4@gmail.com>
2022-05-13 15:26:46 +0530

    Added Cooling period check for OneTap Login

---
+++ src/android/Constants.java
@@ -0,0 +1,19 @@
+package com.devapps;
+
+public class Constants {
+    public static final String PREF_FILENAME                        = "pref.xml";
+
+    public static final String PREF_SHOW_ONE_TAP_UI                 = "pref_show_one_tap_ui";
+    public static final String PREF_COOLING_START_TIME              = "pref_cooling_start_time";
+
+    public static final String CORDOVA_ACTION_ONE_TAP_LOGIN         = "oneTapLogin";
+    public static final String CORDOVA_ACTION_IS_SIGNEDIN           = "isSignedIn";
+    public static final String CORDOVA_ACTION_DISCONNECT            = "disconnect";
+    public static final String CORDOVA_ACTION_SIGNIN                = "signIn";
+    public static final String CORDOVA_ACTION_SIGNOUT               = "signOut";
+
+    public static final String JSON_SUCCESS                         = "success";
+    public static final String JSON_ERROR                           = "error";
+    public static final String JSON_STATUS                          = "status";
+    public static final String JSON_MESSAGE                         = "message";
+}
